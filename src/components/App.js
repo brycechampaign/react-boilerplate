0f3dad541229d46135a71ba94bc56573271348e8
Bryce Champaign <bechampaign@hotmail.com>
2019-10-17 12:05:12 -0400

    Init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return <p>placeholder</p>
+  }
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
